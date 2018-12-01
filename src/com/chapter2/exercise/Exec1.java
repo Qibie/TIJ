fd2cb47c6cfe3555394c785ba91dda3305bdd1af
qibie <472297371@qq.com>
2018-12-01 23:24:43 +0800

    add exercise package and commit Exercise01 code

---
+++ src/com/chapter2/exercise/Exec1.java
@@ -0,0 +1,20 @@
+package com.chapter2.exercise;
+
+/**
+ * @author qibie
+ * @date 2018年12月1日 下午11:18:07
+ */
+public class Exec1 {
+	/** int field */
+	int intField;
+	/** char field */
+	char charField;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Exec1 exec1 = new Exec1();
+		System.out.println("default int field value: " + exec1.intField);
+		System.out.println("default char field value: " + exec1.charField);
+	}
+
+}
