256886ff54e37f55cf2740920cd93a00c1cc7fc4
qibie <472297371@qq.com>
2018-12-01 23:23:28 +0800

    update in 20181201

--- src/com/chapter2/HelloDate.java
+++ src/com/chapter2/HelloDate.java
@@ -1,20 +1,27 @@
-/**
- * Your first java program
- */
+//: object/HelloDate.java
 package com.chapter2;
 
 import java.util.Date;
 
-/**
+/** The first Thinking in Java example program.
+ * Displays a string and today's date.
  * @author qibie
- * @date 2018年12月1日 下午10:13:40   
+ * @author Bruce Eckel
+ * @author www.MindView.net
+ * @date 2018年12月1日 下午10:13:40
+ * @version 4.0   
  */
 public class HelloDate {
-
+	/** Entry point to class & application.
+	 * @param args
+	 * @throws exceptions No exceptions thrown
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, it's: ");		
 		System.out.println(new Date());
 	}
-
-}
+} /* Output: (55% match)
+Hello, it's:
+Wed Oct 05 14:39:36 MDT 2005
+*///:~
